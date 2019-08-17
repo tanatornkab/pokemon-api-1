a3769410f47380ca0cbd26dd07d41bcde0d74420
Hemmarat Wachirahatthapong <hemmarat@go.buu.ac.th>
2019-08-17 11:25:21 +0700

    Support update pokemon

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// https://bit.ly/2KQb0gR
+
 const express = require('express')
 const app = express()
 const port = 3000
@@ -9,6 +11,7 @@ class Pokemon {
         this.name = name
         this.type = type
         this.id = null
+        this.type2 = null
     }
 }
 
@@ -27,6 +30,10 @@ function createPokemon(name, type) {
     return p
 }
 
+function isSufficientParam(v) {
+    return v !== undefined && v !== null && v !== ''
+}
+
 // Generate mock pokemons
 mockPokemon()
 
@@ -38,9 +45,48 @@ app.get('/pokemons', (req, res) => res.send(pokemons))
 
 // POST /pokemons -> add pokemon to list
 app.post('/pokemons', (req, res) => {
+    if (!isSufficientParam(req.body.name)
+        || !isSufficientParam(req.body.type)) {
+
+        res.status(400).send({ error: 'Insufficient parameters: name and type are required parameter' })
+        return
+    }
+
     let p = createPokemon(req.body.name, req.body.type)
     pokemons.push(p)
     res.sendStatus(201)
+})
+
+// GET http://localhost:3000/pokemon/1
+app.get('/pokemon/:id', (req, res) => {
+    let id = req.params.id
+    let p = pokemons[id - 1]
+    res.send(p)
+})
+
+// PUT http://localhost:3000/pokemon/1
+// Add type2
+app.put('/pokemon/:id', (req, res) => {
+    if (!isSufficientParam(req.body.type2)) {
+        res.status(400).send({ error: 'Insufficient parameters: type2 is required parameter' })
+        return
+    }
+
+    if (!isSufficientParam(req.params.id)) {
+        res.status(400).send({ error: 'Insufficient parameters: id is required parameter' })
+        return
+    }
+
+    let id = req.params.id
+    let p = pokemons[id - 1]
+    if (p === undefined) {
+        res.status(400).send({ error: 'Cannot update pokemon: Pokemon is not found' })
+        return
+    }
+
+    p.type2 = req.body.type2
+    pokemons[id - 1] = p
+    res.sendStatus(200)
 })
 
 app.listen(port, () => console.log(`Pokemon API listen on port ${port}`))
