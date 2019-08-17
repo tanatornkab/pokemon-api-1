1401f65da799804235a99cfa09c316c09a970169
Hemmarat Wachirahatthapong <hemmarat@go.buu.ac.th>
2019-08-17 10:14:26 +0700

    Support generate ID for new Pokemon

--- app.js
+++ app.js
@@ -1,22 +1,45 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
 app.use(express.json())
+let pokemons = []
 
+class Pokemon {
+    constructor(name, type) {
+        this.name = name
+        this.type = type
+        this.id = null
+    }
+}
+
+function mockPokemon() {
+    pokemons.push(createPokemon('Pikachu', 'Electric'))
+    pokemons.push(createPokemon('Paras', 'Bug'))
+}
+
+function generateNewId(num) {
+    return num + 1
+}
+
+function createPokemon(name, type) {
+    let p = new Pokemon(name, type)
+    p.id = generateNewId(pokemons.length)
+    return p
+}
+
+// Generate mock pokemons
+mockPokemon()
+
+// Root path
 app.get('/', (req, res) => res.send('Hello World!!!!!!!'))
 
 // GET /pokemons -> list all pokemons http://localhost:3000/pokemons
-let pokemons = [
-    { name: 'Pikachu', type: 'Electric' },
-    { name: 'Paras', type: 'Bug' }
-]
-
 app.get('/pokemons', (req, res) => res.send(pokemons))
 
 // POST /pokemons -> add pokemon to list
 app.post('/pokemons', (req, res) => {
-    pokemons.push(req.body)
+    let p = createPokemon(req.body.name, req.body.type)
+    pokemons.push(p)
     res.sendStatus(201)
 })
 
